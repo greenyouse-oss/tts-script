63f2da6c96f50362a5727d260eb7b7fbdd8d0b43
greenyouse <ed@edbabcock.com>
2024-09-14 17:03:33 -0500

    chore: set up linting

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+const js = require('@eslint/js');
+
+const typescriptParser = require('@typescript-eslint/parser');
+const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
+const prettierPlugin = require('eslint-plugin-prettier');
+
+module.exports = [
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    ignores: ['node_modules/', 'dist/', 'eslint.config.js', ".prettierrc.js"],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...typescriptPlugin.configs.recommended.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+];
